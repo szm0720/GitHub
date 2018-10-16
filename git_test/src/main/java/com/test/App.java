18b276390183d16795b195e4897bc547a6cb6e11
shishimei <shimingming@yunniao.me>
2018-10-16 18:23:04 +0800

    2018年10月16日18:22:57
    2018年10月16日18:23:00描述

--- git_test/src/main/java/com/test/App.java
+++ git_test/src/main/java/com/test/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println();
+
+
+        System.out.println("123");
         System.out.println("2018年10月16日18:08:08");
         System.out.println( "Hello World!" );
     }
