3a85a0dc29ed8bac014f24493206cf0404034bf3
shishimei <shimingming@yunniao.me>
2018-10-16 18:12:30 +0800

    Initial commit

---
+++ git_test/src/main/java/com/test/App.java
@@ -0,0 +1,15 @@
+package com.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println();
+        System.out.println("2018年10月16日18:08:08");
+        System.out.println( "Hello World!" );
+    }
+}
